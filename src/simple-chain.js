40594390b6324875f8a3207961f1c4cce3cf0403
ivan-pogorelov <ivanpogorelov97a@gmail.com>
2021-09-19 16:47:06 +0300

    feat: finished simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,33 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
+  store: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.store.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+
+    this.store.push(`( ${String(value)} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof position !== 'number' || position <= 0 || position > this.getLength()) {
+      this.store.splice(0);
+      throw new Error("You can't remove incorrect link!");
+    } else {
+
+      this.store.splice(position - 1, 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.store = this.store.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let result;
+    result = this.store.join('~~');
+    this.store = [];
+    return result;
   }
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,9 +20,42 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(plainMsg, key) {
+    function isUpperCase(letter) {
+      let l = letter.charCodeAt();
+      if (l >= 65 && l <= 90) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    function isLowerCase(letter) {
+      let l = letter.charCodeAt();
+      if (l >= 97 && l <= 122) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    let cypher = "";
+    for (var i = 0, j = 0; i < plainMsg.length; i++) {
+      let currentLetter = plainMsg[i];
+
+      if (isUpperCase(currentLetter)) {
+        var upperLetter = ((currentLetter.charCodeAt() - 65) + (key[j % key.length].toUpperCase().charCodeAt() - 65)) % 26;
+        cypher += String.fromCharCode(upperLetter + 65);
+        j++;
+      } else if (isLowerCase(currentLetter)) {
+        var lowerLetter = ((currentLetter.charCodeAt() - 97) + (key[j % key.length].toLowerCase().charCodeAt() - 97)) % 26;
+        cypher += String.fromCharCode(lowerLetter + 97);
+        j++;
+      } else {
+        cypher += currentLetter;
+      }
+    }
+    return cypher.toUpperCase();
   }
   decrypt() {
     throw new NotImplementedError('Not implemented');
